13e25384c4882161c7425d988ce9c6fa9986bd34
Ethan Anderson <ethanandy90@gmail.com>
2025-02-10 17:37:56 -0700

    set up users and add some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # This method is used to permit the additional parameters that we want to use in our application.
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
