0c04d08be47abd9168c0c928c7358514d20de88f
Ethan Anderson <ethanandy90@gmail.com>
2025-02-10 17:46:15 -0700

    scaffold server setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_10_235842) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_11_004004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "servers", force: :cascade do |t|
+    t.string "name"
+    t.bigint "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_servers_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_10_235842) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "servers", "users", column: "owner_id"
 end
